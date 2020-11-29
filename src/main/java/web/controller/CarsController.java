37550ad52cd303fc9d4b46aa9d27adaaecb6f5cf
Eugene <mt078658@gmail.com>
2020-11-29 20:33:32 +0300

    now with request params

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,8 +3,10 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,8 @@ import java.util.List;
 public class CarsController {
 
     @GetMapping("/cars")
-    public String printCars(ModelMap model){
+    public String printCars(@RequestParam(name = "count", required = false, defaultValue = "5") int countOfCars, ModelMap model){
+
         List<Car> carList= new ArrayList<>();
 
         carList.add(new Car(1L, "Mercedes", "AMG A35"));
@@ -22,6 +25,8 @@ public class CarsController {
         carList.add(new Car(5L, "MINI", "Clubman"));
 
         model.addAttribute("cars", carList);
+        model.addAttribute("countOfCars", countOfCars);
+
         return "cars";
     }
 }
